95a4b810927caa3d49ce8b9fc07aa5438818d3ce
ConfixsolDev <ConfixsolDev@confixsol.com>
2026-08-31 13:22:08 +0500

    Add Neo4j deployment to storage stack

--- deploy/Enable-QuantLLMStorage.ps1
+++ deploy/Enable-QuantLLMStorage.ps1
@@ -16,6 +16,7 @@ $envFile = Join-Path $PSScriptRoot '.env'
 $composeArgs = @('-f', $compose)
 if (Test-Path $envFile) { $composeArgs += @('--env-file', $envFile) }
 $dbPassword = $env:POSTGRES_PASSWORD
+$neo4jPassword = $env:NEO4J_PASSWORD
 if (-not $dbPassword -and (Test-Path $envFile)) {
     $passwordLine = Get-Content -LiteralPath $envFile | Where-Object {
         $_ -match '^\s*POSTGRES_PASSWORD\s*=\s*(.+?)\s*$'
@@ -25,7 +26,15 @@ if (-not $dbPassword -and (Test-Path $envFile)) {
 if (-not $env:QWEN_TIMESCALE_DSN -and -not $dbPassword) {
     throw 'Set POSTGRES_PASSWORD (or POSTGRES_PASSWORD in deploy/.env) before activation.'
 }
+if (-not $neo4jPassword -and (Test-Path $envFile)) {
+    $neo4jLine = Get-Content -LiteralPath $envFile | Where-Object {
+        $_ -match '^\s*NEO4J_PASSWORD\s*=\s*(.+?)\s*$'
+    } | Select-Object -First 1
+    if ($neo4jLine) { $neo4jPassword = $Matches[1] }
+}
+if (-not $neo4jPassword) { throw 'Set NEO4J_PASSWORD (or NEO4J_PASSWORD in deploy/.env) before activation.' }
 if ($dbPassword) { $env:POSTGRES_PASSWORD = $dbPassword }
+$env:NEO4J_PASSWORD = $neo4jPassword
 docker compose @composeArgs up -d --wait
 if ($LASTEXITCODE -ne 0) { throw 'Storage services failed to start.' }
 
